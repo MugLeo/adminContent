5c6fcbb98684f5dff2cabc3e20483f310b5fb274
LeonardoBarrios <leonardo.barrios@vzorsuite.com>
2023-01-06 16:54:55 -0300

    add model page for card and modifying gitignore for folder migrations

--- CMSWagtail/home/models.py
+++ CMSWagtail/home/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 from wagtail.models import Page
-
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+from wagtail.api import APIField
 
 class HomePage(Page):
-    pass
+    header = RichTextField(blank=True)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('header'),
+        FieldPanel('body'),
+    ]
+
+    api_fields = [
+        APIField('header'),
+        APIField('body'),
+    ]
